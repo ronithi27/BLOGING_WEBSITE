fc5d8fa60334b6db0d28fb48ebfa3ad8aa5ca78f
ronithi27 <ronithi57@gmail.com>
2026-04-19 14:46:28 +0530

    Login fronted Setuo

--- INKIT/Frontend/src/App.jsx
+++ INKIT/Frontend/src/App.jsx
@@ -1,8 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/navbar.component';
+import UserAuthFormPage from './pages/userAuthForm.page';
 
 const App = () => {
+
     return (
-       <NavBar/>
+        <Routes>
+            <Route path='/' element={<NavBar/>}>
+                <Route path='/signin' element={<UserAuthFormPage type='sign-in'/>}/>
+                <Route path='/signup' element={<UserAuthFormPage type='sign-up'/>}/>
+            </Route>
+        </Routes>
     )
 }
 
